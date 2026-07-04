baf188e88acda719739e8cf1203b2b70ad968504
illiasuissakah18 <illiasuissaka18@gmail.com>
2026-07-04 12:46:08 -0700

    Debug MongoDB connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 async function connectDatabase() {
+
     try {
+
+        console.log(
+            "Mongo URI:",
+            process.env.MONGODB_URI ? "Loaded ✅" : "Missing ❌"
+        );
+
         await mongoose.connect(process.env.MONGODB_URI);
 
         console.log("✅ MongoDB Connected Successfully");
@@ -9,11 +16,12 @@ async function connectDatabase() {
     } catch (error) {
 
         console.error("❌ MongoDB Connection Failed");
-        console.error(error.message);
+        console.error(error);
 
         process.exit(1);
 
     }
+
 }
 
 module.exports = connectDatabase;
