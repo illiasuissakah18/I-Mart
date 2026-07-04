a1e97e752d067e9419a3c8ac3a487d9310ca2dc2
illiasuissakah18 <illiasuissaka18@gmail.com>
2026-07-03 22:18:39 -0700

    Fix cartController filename

--- routes/cart.js
+++ routes/cart.js
@@ -7,7 +7,7 @@ const {
     addToCart,
     getCart,
     removeFromCart
-} = require("../controllers/cartController");
+} = require("../controllers/controllers/cartController");
 
 router.post("/add", auth, addToCart);
 router.get("/", auth, getCart);
