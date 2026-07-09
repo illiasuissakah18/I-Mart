0606efcdbf0b50fddd3913a0ea774a74703f7c6e
illiasuissakah18 <illiasuissaka18@gmail.com>
2026-07-09 08:16:32 -0700

    Fix product image upload folder

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,14 +4,21 @@ const path = require("path");
 // Storage engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "uploads/");
+        cb(null, "uploads/products");
     },
+
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+});const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/products");
+    },
+
     filename: function (req, file, cb) {
         cb(null, Date.now() + "-" + file.originalname);
     }
 });
-
-// File filter (images only)
 const fileFilter = (req, file, cb) => {
     const allowedTypes = /jpeg|jpg|png|webp/;
     const extName = allowedTypes.test(path.extname(file.originalname).toLowerCase());
